24269d48bd4c96740206736704a823137d60b6e9
Gustavo Rivero <tavomak@gmail.com>
2021-10-28 00:18:47 -0300

    Loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import { useEffect } from 'react';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'styles/main.scss'
 import TagManager from 'react-gtm-module'
+
+NProgress.configure({ showSpinner: false });
  
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());  
+
 const tagManagerArgs = {
     gtmId: process.env.NEXT_PUBLIC_GTM,
 }
