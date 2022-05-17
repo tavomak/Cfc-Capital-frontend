c3798fdeb18e4b5e709f3e64746b9d3ca35e2c98
Gustavo Rivero <tavomak@gmail.com>
2022-05-17 10:31:12 -0400

    Form active campain

---
+++ data/index.js
@@ -0,0 +1,33 @@
+export const regions = [
+  'Arica y Parinacota',
+  'Tarapacá',
+  'Antofagasta',
+  'Atacama',
+  'Coquimbo',
+  'Valparaíso',
+  'Región Metropolitana de Santiago',
+  'Libertador General Bernardo O’Higgins',
+  'Maule',
+  'Ñuble',
+  'Biobío',
+  'La Araucanía',
+  'Los Ríos',
+  'Los Lagos',
+  'Aysén del General Carlos Ibáñez del Campo',
+  'Magallanes y la Antártica Chilena',
+]
+
+export const business = [
+  'Construcción',
+  'Climatización',
+  'Minería',
+  'Transporte',
+  'Telecomunicaciones',
+  'Servicios transitorios',
+  'Servicios de vigilancia',
+  'Aseo industrial',
+  'Industria de la salud',
+  'Textil',
+  'Mantenimiento industrial',
+  'Otro'
+]
