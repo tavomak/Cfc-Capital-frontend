a809fc7ffffe6e466cd7b5820bc59a637c7b5573
Gustavo Rivero <tavomak@gmail.com>
2024-08-06 10:54:55 -0400

    feature(flow) Form Stepper image

--- pages/canal-de-denuncias/index.js
+++ pages/canal-de-denuncias/index.js
@@ -7,7 +7,14 @@ const LawForm = () => (
     title="Canal de denuncias"
     description="Este es el canal de denuncias dispuesto exclusivamente para recibir denuncias por Responsabilidad Penal de la Persona Jurídica Ley 20.393 y Denuncias asociadas a Ley 21.643 (Ley Karin)"
   >
-    <Hero image="ley" alt="Canal de denuncias" />
+    <Hero
+      image="ley"
+      alt="Canal de denuncias"
+      heroImages={{
+        desktop: '/denuncias.jpg',
+        mobile: '/denuncias-mobile.jpg',
+      }}
+    />
     <section className="container-flluid">
       <div className="container">
         <div className="row pt-5 align-items-center">
@@ -30,7 +37,7 @@ const LawForm = () => (
                 </li>
               </ol>
               <p className="">La comisión de los delitos establecidos en la Ley N° 20.393, de Lavado de Activos, Financiamiento del Terrorismo y/o Cohecho a funcionario público nacional o extranjero; ejercicio de sus funciones y en beneficio de esta institución.</p>
-              <p className="">La Ley 21.643 acoge los eventos relacionados con el Abuso, Laboral, Acoso Sexual o de Violencia en el Trabajo, dentro de los colaboradores y nuestros proveedores y asesores externos</p>
+              <p className="">La Ley 21.643 acoge los eventos relacionados con el Abuso Laboral, Acoso Sexual o de Violencia en el Trabajo, dentro de los colaboradores y nuestros proveedores y asesores externos</p>
               <p className="">
                 Para nuestra compañía, son importantes los valores con los que desarrolla sus
                 {' '}
@@ -42,13 +49,15 @@ const LawForm = () => (
                 {' '}
                 un canal de comunicación adicional a los ya existentes (Canal de Denuncias) y que
                 {' '}
-                es de carácter con la empresa y que tenga información de alguna actividad
+                es de carácter confidencial y anónimo, según corresponda para toda persona que
                 {' '}
-                desarrollada por la empresa o algún colaborador, proveedor de la misma, que
+                tenga información de alguna actividad desarrollada por la empresa o algún
                 {' '}
-                pudiese implicar una violación a nuestros valores, leyes, normativa, códigos,
+                colaborador, proveedor de la misma, que pudiese implicar una violación a nuestros
                 {' '}
-                reglamentos o políticas internas, y principios éticos en general.
+                valores, leyes, normativa, códigos,reglamentos o políticas internas, y principios
+                {' '}
+                éticos en general.
               </p>
               <p>
                 <strong className="fw-bold display-font">
