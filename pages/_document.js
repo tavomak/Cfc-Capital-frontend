c8ffc18301aa509f16a50a9756c611da16678cc7
Gustavo Rivero <tavomak@gmail.com>
2024-12-04 17:33:09 -0300

    fix(metadata): Search console verification

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="NWNJ1QfYOX9Bl4nmm9OiOurC8hIY1tyEza_77_R8XfM"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
