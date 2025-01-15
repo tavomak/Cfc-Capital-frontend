9d3b0a4a1438af65cbd9ffa1c085d79531152688
cesarmartinez-estela <cesar.martinez@estelaestudio.com>
2025-01-15 12:40:25 -0400

    Revert "feat: update signature"

--- components/Molecules/Signature/Signature.js
+++ components/Molecules/Signature/Signature.js
@@ -45,7 +45,7 @@ const Signature = ({ data, setData }) => {
 
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="w-11/12 lg:w-1/2">
+      <div className="w-1/2">
         <h5 className="mb-10 font-bold text-center text-blue">
           Previsualización de firma de correo
         </h5>
@@ -60,279 +60,241 @@ const Signature = ({ data, setData }) => {
               fontSize: 'medium',
               fontFamily: 'Arial',
               width: '100%',
-              maxWidth: '800px',
+              maxWidth: '400px',
             }}
           >
             <tbody>
               <tr>
+                <td height="20" aria-label="Espaciador vertical" />
+              </tr>
+
+              <tr>
                 <td
-                  style={{
-                    verticalAlign: 'middle',
-                    width: '50%',
-                    textAlign: 'center',
-                  }}
+                  style={{ verticalAlign: 'middle' }}
+                  aria-label="Contenedor de nombre y posición"
                 >
                   <table
                     cellPadding="0"
                     cellSpacing="0"
                     border="0"
                     style={{
+                      verticalAlign: 'middle',
+                      fontSize: 'medium',
+                      fontFamily: 'Arial',
                       width: '100%',
+                      lineHeight: '1',
                     }}
                   >
-                    <tr>
-                      <td
-                        style={{
-                          paddingBottom: '20px',
-                        }}
-                      >
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
-                          src="./cfc-logo-vertical.png"
-                          style={{
-                            display: 'block',
-                            maxWidth: '200px',
-                            margin: '0 auto',
-                          }}
-                          alt="CFC Capital"
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td
-                        style={{
-                          paddingBottom: '20px',
-                        }}
-                      >
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
-                          src="./Logo-20años.svg"
-                          alt="20 años"
-                          width="37"
-                          height="30"
-                          style={{
-                            width: 'auto',
-                            height: '30px',
-                            display: 'inline',
-                          }}
-                        />
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
-                          width="63"
-                          height="30"
-                          src="./Logo-40hrs.svg"
-                          alt="40 horas"
-                          style={{
-                            width: 'auto',
-                            height: '30px',
-                            margin: '0 10px',
-                            display: 'inline',
-                          }}
-                        />
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
-                          width="45"
-                          height="30"
-                          src="./Logo-Efa.svg"
-                          alt="EFA"
-                          style={{
-                            width: 'auto',
-                            height: '30px',
-                            display: 'inline',
-                          }}
-                        />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <p
-                          style={{
-                            margin: 0,
-                            fontSize: '10px',
-                            color: '#7f7f7f',
-                          }}
+                    <tbody>
+                      <tr>
+                        <td
+                          align="left"
+                          width="200"
+                          style={{ verticalAlign: 'middle' }}
+                          aria-label="Contenedor de logo"
                         >
-                          Av. El Bosque 92, piso 11,
-                          <br />
-                          Las Condes, Santiago, Chile
-                        </p>
-                      </td>
-                    </tr>
-                  </table>
-                </td>
-
-                <td
-                  color="#d3d3d3"
-                  direction="vertical"
-                  style={{
-                    backgroundColor: '#b1a3c6',
-                    height: '100%',
-                  }}
-                  aria-label="Separador vertical"
-                >
-                  <span
-                    style={{
-                      display: 'block',
-                      width: '2px',
-                    }}
-                  />
-                </td>
-
-                <td
-                  style={{
-                    width: '100%',
-                    paddingLeft: '20px',
-                  }}
-                >
-                  <table cellPadding="0" cellSpacing="0" border="0">
-                    <tr>
-                      <td>
-                        <p
-                          style={{
-                            margin: 0,
-                          }}
+                          <a
+                            href="https://www.cfccapital.cl/"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            {/* eslint-disable-next-line @next/next/no-img-element */}
+                            <img
+                              src="./cfc-logo.jpg"
+                              role="presentation"
+                              width="160"
+                              style={{ display: 'block', maxWidth: '160px' }}
+                              alt="CFCCapital"
+                            />
+                          </a>
+                        </td>
+                        <td
+                          align="left"
+                          width="200"
+                          style={{ verticalAlign: 'middle' }}
                         >
-                          <b>
+                          <p style={{ margin: 0 }}>
+                            <b>
+                              <span
+                                style={{
+                                  color: '#623482',
+                                  fontSize: '15px',
+                                }}
+                              >
+                                {data.name ? (
+                                  data.name
+                                ) : (
+                                  <span className={styles.skeletonContainer}>
+                                    <span
+                                      className={styles.skeleton}
+                                      style={{ height: '35px' }}
+                                    />
+                                  </span>
+                                )}
+                              </span>
+                            </b>
+                          </p>
+                          <p style={{ margin: 0 }}>
                             <span
-                              style={{
-                                color: '#623482',
-                                fontSize: '15px',
-                              }}
+                              style={{ fontSize: '12px', color: '#00ABC8' }}
                             >
-                              {data.name ? (
-                                data.name
+                              {data.position ? (
+                                data.position
                               ) : (
                                 <span className={styles.skeletonContainer}>
                                   <span
                                     className={styles.skeleton}
-                                    style={{ height: '35px' }}
+                                    style={{
+                                      animationDelay: '0.1s',
+                                      height: '20px',
+                                    }}
                                   />
                                 </span>
-                              )}{' '}
+                              )}
                             </span>
-                          </b>
-                        </p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td
-                        style={{
-                          paddingBottom: '15px',
-                        }}
-                      >
-                        <p
-                          style={{
-                            margin: 0,
-                          }}
+                          </p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+
+              <tr>
+                <td height="10" aria-label="Espaciador vertical" />
+              </tr>
+              <tr>
+                <td
+                  color="#d3d3d3"
+                  direction="horizontal"
+                  height="1"
+                  style={{
+                    width: '100%',
+                    borderLeft: 'none',
+                    backgroundColor: '#d3d3d3',
+                  }}
+                  aria-label="Separador horizontal"
+                >
+                  <span
+                    style={{
+                      borderBottom: '1px solid #d3d3d3',
+                      display: 'block',
+                      width: '100%',
+                    }}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td height="10" aria-label="Espaciador vertical" />
+              </tr>
+
+              <tr>
+                <td style={{ verticalAlign: 'middle' }}>
+                  <table
+                    cellPadding="0"
+                    cellSpacing="0"
+                    border="0"
+                    style={{
+                      verticalAlign: 'middle',
+                      fontSize: 'medium',
+                      fontFamily: 'Arial',
+                      width: '100%',
+                    }}
+                  >
+                    <tbody>
+                      <tr>
+                        <td
+                          align="left"
+                          width="200"
+                          style={{ verticalAlign: 'middle' }}
+                          aria-label="Contenedor de iconos"
                         >
-                          <span
+                          {/* eslint-disable-next-line @next/next/no-img-element */}
+                          <img
+                            src="icon20.png"
+                            alt="20 años"
+                            width="37"
+                            height="30"
                             style={{
+                              width: 'auto',
+                              height: '30px',
+                              display: 'inline',
+                            }}
+                          />
+                          {/* eslint-disable-next-line @next/next/no-img-element */}
+                          <img
+                            width="63"
+                            height="30"
+                            src="icon40.png"
+                            alt="40 horas"
+                            style={{
+                              width: 'auto',
+                              height: '30px',
+                              marginLeft: '10px',
+                              marginRight: '10px',
+                              display: 'inline',
+                            }}
+                          />
+                          {/* eslint-disable-next-line @next/next/no-img-element */}
+                          <img
+                            width="45"
+                            height="30"
+                            src="icon_efa.png"
+                            alt="EFA"
+                            style={{
+                              width: 'auto',
+                              height: '30px',
+                              display: 'inline',
+                            }}
+                          />
+                        </td>
+                        <td align="left" width="200">
+                          <p
+                            style={{
+                              margin: 0,
+                              color: '#7f7f7f',
                               fontSize: '12px',
-                              color: '#00ABC8',
                             }}
                           >
-                            {data.position ? (
-                              data.position
-                            ) : (
-                              <span className={styles.skeletonContainer}>
-                                <span
-                                  className={styles.skeleton}
-                                  style={{
-                                    animationDelay: '0.1s',
-                                    height: '20px',
-                                  }}
-                                />
-                              </span>
-                            )}
-                          </span>
-                        </p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td
-                        style={{
-                          paddingBottom: '15px',
-                        }}
-                      >
-                        <p
-                          style={{
-                            margin: 0,
-                            color: '#7f7f7f',
-                            fontSize: '14px',
-                          }}
-                        >
-                          <span>+56 </span>
-                          {data.phone
-                            ? formatPhoneNumberString(data.phone)
-                            : ''}
-                        </p>
-                        <p
-                          style={{
-                            margin: 0,
-                            color: '#7f7f7f',
-                            fontSize: '14px',
-                          }}
-                        >
-                          {data.mobile && (
-                            <>
-                              <span>+56 </span>
-                              {formatPhoneNumberString(data.mobile)}
-                            </>
+                            {data.phone
+                              ? formatPhoneNumberString(data.phone)
+                              : ''}
+                          </p>
+                          {data.phone && (
+                            <p
+                              style={{
+                                margin: 0,
+                                color: '#7f7f7f',
+                                fontSize: '12px',
+                              }}
+                            >
+                              {data.mobile && (
+                                <>
+                                  <span>56 </span>
+                                  {formatPhoneNumberString(data.mobile)}
+                                </>
+                              )}
+                            </p>
                           )}
-                        </p>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <table cellPadding="0" cellSpacing="0" border="0">
-                          <tr>
-                            <td
-                              style={{
-                                paddingTop: '2px',
-                                paddingRight: '15px',
-                              }}
-                            >
-                              <a href="!#">
-                                {/* eslint-disable-next-line @next/next/no-img-element */}
-                                <img
-                                  src="./IG-logo.svg"
-                                  alt="Instagram"
-                                  width="24"
-                                  height="24"
-                                />
-                              </a>
-                            </td>
-                            <td
-                              style={{
-                                paddingRight: '15px',
-                              }}
-                            >
-                              <a href="!#">
-                                {/* eslint-disable-next-line @next/next/no-img-element */}
-                                <img
-                                  src="./Linke-logo.svg"
-                                  alt="LinkedIn"
-                                  width="24"
-                                  height="24"
-                                />
-                              </a>
-                            </td>
-                            <td>
-                              <a href="!#">
-                                {/* eslint-disable-next-line @next/next/no-img-element */}
-                                <img
-                                  src="./FB-logo.svg"
-                                  alt="Facebook"
-                                  width="15"
-                                  height="15"
-                                />
-                              </a>
-                            </td>
-                          </tr>
-                        </table>
-                      </td>
-                    </tr>
+                        </td>
+                      </tr>
+                    </tbody>
                   </table>
+                </td>
+              </tr>
+
+              <tr>
+                <td height="15" aria-label="Espaciador vertical" />
+              </tr>
+
+              <tr>
+                <td colSpan="2" align="left">
+                  <p>
+                    <span style={{ fontSize: '10px', color: '#7f7f7f' }}>
+                      AV. El Bosque Central 92, piso 11, Las Condes, Santiago,
+                      Chile
+                    </span>
+                  </p>
                 </td>
               </tr>
             </tbody>
