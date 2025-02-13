8e750aa1e57529240194be6511fffb442aeeefd5
cesarmartinez-estela <cesar.martinez@estelaestudio.com>
2025-02-13 12:30:24 -0400

    chore(newCard) border rounded image deleted

--- components/Molecules/NewCard/NewCard.js
+++ components/Molecules/NewCard/NewCard.js
@@ -3,13 +3,10 @@ import Image from 'next/image';
 
 const NewCard = ({ title, slug, image, author, excerpt, tags }) => (
   <Card
-    containerClassName="p-2 w-3/4 lg:w-11/12 mx-auto"
+    containerClassName="p-2 w-3/4 lg:w-11/12 mx-auto mb-4"
     cardClassName="flex flex-col min-w-72 group p-4 rounded-xl h-full hover:shadow-none"
     header={
-      <a
-        href={`/prensa/${slug}`}
-        className="overflow-hidden min-h-64 rounded-3xl group"
-      >
+      <a href={`/prensa/${slug}`} className="overflow-hidden min-h-64 group">
         <Image
           className="transition scale-100 group-hover:scale-110"
           src={image}
