44ad7608f877a6faa14db8075b62284fcf9919b0
cesarmartinez-estela <cesar.martinez@estelaestudio.com>
2025-04-16 10:53:06 -0400

    fix(pagination) improve number btn functionality

--- components/Molecules/Pagination/Pagination.js
+++ components/Molecules/Pagination/Pagination.js
@@ -11,32 +11,45 @@ const Pagination = ({ currentPage, totalPages, basePath }) => {
     <nav>
       <ul className="flex justify-center gap-1">
         {currentPage > 1 && (
-          <li className="px-3 py-2 ml-2 text-sm text-center transition-all border rounded-full shadow-sm border-slate-300 hover:shadow-lg text-medium-purple hover:text-white hover:bg-medium-purple hover:border-medium-purple disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-            <Link href={`${basePath}/${currentPage - 1}`}>Prev</Link>
-          </li>
+          <Link
+            href={`${basePath}/${currentPage - 1}`}
+            className="px-3 py-2 ml-2 text-sm text-center transition-all border rounded-full shadow-sm border-slate-300 hover:shadow-lg text-medium-purple hover:text-white hover:bg-medium-purple hover:border-medium-purple disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            <button type="button" className="block w-full h-full">
+              Prev
+            </button>
+          </Link>
+        )}
+        {pageNumbers.map((number) =>
+          currentPage === number ? (
+            <button
+              key={number}
+              type="button"
+              disabled
+              className="min-w-9 rounded-full border border-slate-300 py-2 px-3.5 text-center text-sm shadow-sm bg-medium-purple text-white border-medium-purple pointer-events-none opacity-50 ml-2"
+            >
+              {number}
+            </button>
+          ) : (
+            <Link
+              key={number}
+              href={`${basePath}/${number}`}
+              className="min-w-9 hover:border-medium-purple rounded-full border border-slate-300 py-2 px-3.5 text-center text-sm transition-all shadow-sm hover:bg-medium-purple hover:text-white hover:shadow-lg text-medium-purple ml-2"
+            >
+              {number}
+            </Link>
+          )
         )}
 
-        {pageNumbers.map((number) => (
-          <li
-            key={number}
-            className={`min-w-9 hover:border-medium-purple rounded-full border border-slate-300 py-2 px-3.5 text-center text-sm transition-all shadow-sm
-              ${currentPage === number ? 'bg-medium-purple text-white border-medium-purple' : 'hover:bg-medium-purple hover:text-white'}
-              hover:shadow-lg text-medium-purple  ml-2`}
-          >
-            {currentPage === number ? (
-              <button type="button" disabled>
-                {number}
-              </button>
-            ) : (
-              <Link href={`${basePath}/${number}`}>{number}</Link>
-            )}
-          </li>
-        ))}
-
         {currentPage < totalPages && (
-          <li className="px-3 py-2 ml-2 text-sm text-center transition-all border rounded-full shadow-sm border-slate-300 hover:shadow-lg text-medium-purple hover:text-white hover:bg-medium-purple hover:border-medium-purple disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-            <Link href={`${basePath}/${currentPage + 1}`}>Next</Link>
-          </li>
+          <Link
+            href={`${basePath}/${currentPage + 1}`}
+            className="px-3 py-2 ml-2 text-sm text-center transition-all border rounded-full shadow-sm border-slate-300 hover:shadow-lg text-medium-purple hover:text-white hover:bg-medium-purple hover:border-medium-purple disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            <button type="button" className="block w-full h-full">
+              Next
+            </button>
+          </Link>
         )}
       </ul>
     </nav>
