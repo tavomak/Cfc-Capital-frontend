ff7b5aeb15448d031388d8a3f5d842d0f10c82ad
Gustavo Rivero <tavomak@gmail.com>
2025-07-14 11:21:14 -0400

    fix(Develop): Image source

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    deviceSizes: [768, 1440],
+    imageSizes: [32, 128],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'media.graphassets.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'us-east-1.cdn.hygraph.com',
         port: '',
         pathname: '/**',
       },
